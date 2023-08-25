dfc7b73eac2dba7a2a3e536df160ad6ac0b1a506
Pharm-Engr-Amy <lawalaminat144@gmail.com>
2023-08-25 16:18:04 +0100

    This is my first commit

---
+++ data_collection.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+name = input("please enter your name:")
+age = input ("please enter your age:")
+phone_number = input("please enter your phone number:")
+email_address = input ("please enter your email address:")
+
+
+print("Dear " + (name) + ", your are " + (age) +" years old, " + " and your other details are: " + (email_address) + ", "+ (phone_number))
+
