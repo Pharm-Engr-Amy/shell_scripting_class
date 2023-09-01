223d7035687e865176d4b241299d7aefad0e5394
Pharm-Engr-Amy <lawalaminat144@gmail.com>
2023-09-01 16:35:14 +0100

    assignment

---
+++ assignment.py
@@ -0,0 +1,26 @@
+#This is my first assignment by Lawal Aminat
+
+#A code that calculates the area of a circle
+
+print("==========area of a circle========")
+r=int(input("please enter radius of a circle = "))
+a=3.142*r**2
+print(a)
+      
+print("===========simple interest========")
+p=int(input("please enter principal = "))
+r=float(input("please enter rate = "))
+t=float(input("please enter time = "))
+
+si=(p*r*t)//100
+print(si)
+
+print("==========area of a triangle========")
+b=int(input("please enter base = "))
+h=int(input("please enter height = "))
+
+a=(b*h)//2
+print(a)
+
+
+
